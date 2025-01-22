c0308b7f30e5acff975fc89432bfdc883684c493
Raushan Raj <164464654+raju-raushan@users.noreply.github.com>
2025-01-22 15:15:48 +0530

    Update my_route.php

--- my_route.php
+++ my_route.php
@@ -131,11 +131,11 @@
 </section>
 <br/><br/><br/>
 </main>
-<script src="../js/jquery.min.js"></script>
-<script src="../js/bootstrap.min.js"></script>
-<script src="../js/jquery.sticky.js"></script>
-<script src="../js/click-scroll.js"></script>
-<script src="../js/custom.js"></script>
-<script src="../js/visitor.js"></script>
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.sticky.js"></script>
+<script src="js/click-scroll.js"></script>
+<script src="js/custom.js"></script>
+<script src="js/visitor.js"></script>
 </body>
 </html>
